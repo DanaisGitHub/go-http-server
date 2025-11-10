585c3eef1eb11cefe17eb8cf611a898bacfb57d7
Danai <Dan.Zerai22@gmail.com>
2025-11-10 08:27:31 +0000

    parse nearly done
    just need to add multiline functionality to parse

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -3,54 +3,64 @@ package headers
 import (
 	"bytes"
 	"fmt"
-	"maps"
-	"slices"
+	"strings"
 )
 
-var SEPARATOR = []byte("\r\n")
+var NEWLINE = []byte("\r\n")
 
 type Headers map[string]string
 
-// input I think will look like
-// header:
-	// field:value
-	// field:value
-	// ...
+func (h Headers) Get(name string) string {
+	return h[strings.ToLower(name)]
+}
+
+func (h Headers) Set(field, value string) {
+	field = strings.ToLower(field)
+	h[field] = value
+}
+
+func isToken(fieldName string) bool {
+	return true
+}
+
+// this needs to be able to parse multiple lines
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
 	// need to get a full line of the data
 
-	n = len(data)
+	n = 0
 	done = false
 	err = nil
+	for {
+		idx := bytes.Index(data[n:], NEWLINE)
 
-	idx := bytes.Index(data, SEPARATOR)
+		if idx == -1 {
+			break
+		}
+		if idx == 0 { // no header
+			done = true
+			break
+		}
 
-	if idx == 0 { // no header
-		return n, done, nil
+		currData := data[n:idx]
+		n += idx + len(NEWLINE)
+
+		parts := bytes.SplitN(currData, []byte(":"), 2)
+		if len(parts) != 2 {
+			return 0, false, fmt.Errorf("malformed header")
+		}
+		field := parts[0]
+		value := bytes.TrimSpace(parts[1])
+		if bytes.HasSuffix(field, []byte(" ")) {
+			return 0, false, fmt.Errorf("field of header malformed, space(s) between field & :")
+		}
+
+		if !isToken(string(field)) {
+			return 0, false, fmt.Errorf("field name %s is not valid token", field)
+		}
+
+		h.Set(string(field), string(value))
 	}
-	if idx == len(data)-len(SEPARATOR)-1 {
-		done = true
-	}
-
-	parts := bytes.SplitN(data, []byte(":"), 2)
-	if len(parts) != 2 {
-		return 0, false, fmt.Errorf("malformed header")
-	}
-	field := parts[0]
-	value:=bytes.TrimSpace(parts[1])
-	if bytes.HasSuffix(field, []byte(" ")) {
-		return 0, false, fmt.Errorf("field of header malformed, space(s) between field & :")
-
-	}
-
-	header:=[]string{
-		string(field),string(value),
-	}
-
-	maps.Insert(h,slices.All(header))
-
 	return n, done, err
-
 }
 
 func NewHeaders() Headers {
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -7,17 +7,17 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestRequestLineParse(t *testing.T) {
+func TestRequestLineParse1(t *testing.T) {
 
 	// Test: Valid single header
 	headers := NewHeaders()
-	data := []byte("Host: localhost:42069\r\n\r\n")
+	data := []byte("HOst: localhost:42069\r\n\r\n")
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
-	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "localhost:42069", headers.Get("hOsT"))
 	assert.Equal(t, 23, n)
-	assert.False(t, done)
+	assert.True(t, done)
 
 	// Test: Invalid spacing header
 	headers = NewHeaders()
@@ -26,5 +26,34 @@ func TestRequestLineParse(t *testing.T) {
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
+}
+func TestErronous(t *testing.T) {
+
+	// Test: Multi-field values
+	headers := NewHeaders()
+	data := []byte("Host: localhost:42069\r\n Foo: Bar \r\n\r\n")
+	n, done, err := headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, "Bar", headers["Foo"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+	// // Test: Empty
+	// headers = NewHeaders()
+	// data = []byte("\r\n\r\n       Host: localhost:42069       \r\n\r\n")
+	// n, done, err = headers.Parse(data)
+	// require.NoError(t, err)
+	// assert.NotEqual(t, 0, n)
+	// assert.False(t, done)
+
+	// // Test: Malformed
+	// headers = NewHeaders()
+	// data = []byte("Host     \r\n\r\n")
+	// n, done, err = headers.Parse(data)
+	// require.Error(t, err)
+	// assert.Equal(t, 0, n)
+	// assert.False(t, done)
 
 }
